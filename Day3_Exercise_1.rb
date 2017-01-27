1106f128ec02781a1caef2cf96cdd7359fe75f02
benja <benjabuddy91@gmail.com>
2017-01-27 15:12:35 +0800

    Add Day3_Exercise_1.rb

---
+++ Day3_Exercise_1.rb
@@ -0,0 +1,11 @@
+include Math
+
+def is_power_of_two(n)
+	return true if (Math.sqrt(n) % 1).zero?
+	return false
+end
+
+puts is_power_of_two(30)
+puts is_power_of_two(36)
+puts is_power_of_two(40)
+puts is_power_of_two(49)
